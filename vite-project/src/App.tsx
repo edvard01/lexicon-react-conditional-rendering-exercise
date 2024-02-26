5a628f64c8f2990e6acef0f8db3981ca58a1ca85
edvard01 <frejaclueless@gmail.com>
2024-02-26 15:11:35 +0100

    Can now change p element with the input

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,7 +1,42 @@
+import { useState } from "react";
+
 export function App() {
+  //  const [isLoading, setIsLoading] = useState(true);
+  const [isEditing, setIsEditing] = useState(false);
+  const [todoValue, setTodoValue] = useState("Sleep 4 hours.");
+
+  // if (isLoading) {
+  //   setTimeout(() => {
+  //     setIsLoading(false);
+  //   }, 3000);
+
+  //   return (
+  //     <>
+  //       <h1>Loading...</h1>
+  //     </>
+  //   );
+  // }
+
+  const handleOnClick = () => {
+    setIsEditing((preVal) => !preVal);
+  };
+
   return (
     <>
-      <h1>App Component</h1>
+      <h1>App Component is ready</h1>
+      <p>- TODO -</p>
+      <div>
+        {isEditing ? (
+          <input
+            type="text"
+            value={todoValue}
+            onChange={(e) => setTodoValue(e.target.value)}
+          />
+        ) : (
+          <p>{todoValue}</p>
+        )}
+      </div>
+      <button onClick={handleOnClick}>{isEditing ? "Cancel" : "Edit"}</button>
     </>
   );
 }
